3640b27c0ba2f8412bc49cf8f7460113afe86d47
Lucas Timotheo Sanches <lucas.t.s.carneiro@gmail.com>
2024-05-24 21:06:16 -0300

    Adde new game bttn

---
+++ 2048/include/ui.hpp
@@ -0,0 +1,52 @@
+#ifndef SURGE_2048_UI_HPP
+#define SURGE_2048_UI_HPP
+
+#include "player/integer_types.hpp"
+#include "type_aliases.hpp"
+
+namespace s2048::ui {
+
+/*
+ * Detect if a point is inside a rectangle
+ */
+static inline auto point_in_rect(const glm::vec2 &point, const glm::vec4 &rect) noexcept {
+  const auto x{point[0]};
+  const auto y{point[1]};
+
+  const auto x0{rect[0]};
+  const auto xf{rect[0] + rect[2]};
+
+  const auto y0{rect[1]};
+  const auto yf{rect[1] + rect[3]};
+
+  const auto in_x_range{x0 < x && x < xf};
+  const auto in_y_range{y0 < y && y < yf};
+
+  return in_x_range && in_y_range;
+}
+
+struct ui_state {
+  GLFWwindow *window;
+  surge::i32 active;
+  surge::i32 hot;
+};
+
+struct draw_data {
+  glm::vec2 pos;
+  glm::vec2 scale;
+  float z;
+  float alpha;
+};
+
+struct button_skin {
+  GLuint64 handle_release;
+  GLuint64 handle_select;
+  GLuint64 handle_press;
+};
+
+auto button(surge::i32 id, ui_state &state, draw_data &dd, sdb_t &sdb,
+            const button_skin &bs) noexcept -> bool;
+
+} // namespace s2048::ui
+
+#endif // SURGE_2048_UI_HPP
--- 2048/src/board.cpp
+++ 2048/src/board.cpp
@@ -1,6 +1,7 @@
 #include "board.hpp"
 
 #include "player/window.hpp"
+#include "ui.hpp"
 
 auto s2048::board::load(GLFWwindow *, tdb_t &tdb, sdb_t &sdb) noexcept
     -> std::optional<surge::error> {
@@ -15,6 +16,10 @@ auto s2048::board::load(GLFWwindow *, tdb_t &tdb, sdb_t &sdb) noexcept
   texture::create_info ci{};
   ci.filtering = renderer::texture_filtering::anisotropic;
   tdb.add(ci, "resources/board.png");
+
+  // New game bttn
+  tdb.add(ci, "resources/button_press.png");
+  tdb.add(ci, "resources/button_release.png");
 
   return {};
 }
@@ -34,11 +39,23 @@ void s2048::board::update(double, GLFWwindow *window, const tdb_t &tdb, sdb_t &s
 
   // Texture handles
   static const auto bckg_handle{tdb.find("resources/board.png").value_or(0)};
+  static const auto new_game_press{tdb.find("resources/button_press.png").value_or(0)};
+  static const auto new_game_release{tdb.find("resources/button_release.png").value_or(0)};
 
   // Background model
   const auto [ww, wh] = window::get_dims(window);
   const auto bckg_model{sprite::place(glm::vec2{0.0f}, glm::vec2{ww, wh}, 0.1f)};
   sdb.add(bckg_handle, bckg_model, 1.0);
+
+  // UI code
+  static s2048::ui::ui_state uist{window, -1, -1};
+
+  // New game bttn
+  s2048::ui::draw_data dd{glm::vec2{358.0f, 66.0f}, glm::vec2{138.0f, 40.0f}, 0.2f, 1.0f};
+  s2048::ui::button_skin skins{new_game_release, new_game_release, new_game_press};
+  if (s2048::ui::button(__COUNTER__, uist, dd, sdb, skins)) {
+    log_info("Reset game points");
+  }
 }
 
 void s2048::board::draw(sdb_t &sdb) noexcept { sdb.draw(); }
